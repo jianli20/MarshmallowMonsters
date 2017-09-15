3d6f1ebf66355194f241f44f073531b0280cee4b
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-09-15 12:11:51 -0600

    Created toString method, returns the description of the monster

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,33 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	public MarshmallowMonster()
+	{
+		//initializes everything to 0, false or null.
+	}
+	
+	public MarshmallowMonster(String name, int eyeCount, int arms, int tenaclecount, boolean hasBloop)
+	{
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleAmount;
+		this.hasBloop = hasBloop;
+	}
+	
+	public String toString()
+	{
+		String description = "I am a monster, my name is " + name + ", I have" + eyeCount + "eyes and only" ;
+		description += tentacleAmount +  "tentacles but I have" + armCount + " arms!." + hasBloop + " tells you";
+		description += " if I have a bloop";
+		return description;
+	}
 }
