d26a55158e9df52ffb3e2637a3c6b3f1f6986e7f
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-09-21 12:49:51 -0600

    Replaced user input from console based to popup based, created a Helper method for tentacles and arms

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -35,35 +35,61 @@ public class MonsterController
 	{
 		Scanner myScanner = new Scanner(System.in);
 //		System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, please type in how many ");
-		int consumed; 
-		String answer = popup.getResponse(currentMonster.getName() + " wants to know how many eyes you want to eat, please type in how many");
+		int consumed =0;
+		String response = popup.getResponse(currentMonster.getName() + " wants to know how many eyes you want to eat, please type in how many");
 		
-		consumed = Integer.parseInt(answer);
+		if(isValidInteger(response))
+		{
+			consumed = Integer.parseInt(response);
+		}
 		
 //		int consumed = myScanner.nextInt();
 		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
-		System.out.println(currentMonster);		
-		System.out.println("How many arms are you interested in eating?, I have "  + currentMonster.getarmCount());
+//		System.out.println(currentMonster);		
+//		System.out.println("How many arms are you interested in eating?, I have "  + currentMonster.getarmCount());
 		//consumed = myScanner.nextInt();
-		int armEat = myScanner.nextInt();
+		
+		popup.displayText(currentMonster.toString());
+		popup.getResponse(currentMonster.getName() + " How many arms are you interested in eating?, I have "  + currentMonster.getarmCount());
+		
+//		int armEat = myScanner.nextInt();
+		int armEat = 0;
+		if(isValidInteger(response))
+		{
+			consumed = Integer.parseInt(response);
+		}
 		
 		if (armEat == 0)
 		{
-			System.out.println("Not hungry?  so sad.....");
+//			System.out.println("Not hungry?  so sad.....");
+			popup.displayText("Not hungry?   so sad.....");
 		}
 		else if(armEat < 0)
 		{
-			System.out.println("Math is hard for you- it's impossible to eat a negative amount");
+//			System.out.println("Math is hard for you- it's impossible to eat a negative amount");
+			popup.displayText("Math is hard for you- it's impossible to eat a negative amount");
 		}
 		else if(armEat - currentMonster.getarmCount() > 0)
 		{
-			System.out.println("You are not allowed to eat more than exist on me :(");
+//			System.out.println("You are not allowed to eat more than exist on me :(");
+			popup.displayText("You are not allowed to eat more than exist on me :(");
 		}
 		else
 		{
-			System.out.println("OK, now I have this many arms " + currentMonster.getarmCount());
+//			System.out.println("OK, now I have this many arms " + currentMonster.getarmCount());
+			popup.displayText("Ok, now I have this many arms " + currentMonster.getarmCount());
 		}
 		
+		double food = 0.0;
+		String tentacleResponse = popup.getResponse("");
+		if(isValidDouble(tentacleResponse))
+		{
+			food = Double.parseDouble(tentacleResponse);
+		}
+		if(food == currentMonster.getTentacleAmount())
+		{
+			popup.displayText("You ate all my tentacles!");
+		}
 		
 		popup.displayText("Hi there ready to play???");
 		String answer1 = popup.getResponse("What is the air speed of a coconut laden swallow?");
@@ -75,7 +101,7 @@ public class MonsterController
 	}
 
 	//Helper methods
-	private boolean isValidIneger(String sample)
+	private boolean isValidInteger(String sample)
 	{
 		boolean valid = false;
 		
@@ -92,7 +118,21 @@ public class MonsterController
 		return valid;
 	}
 
-
+	private boolean isValidDouble(String sampleDouble)
+	{
+		boolean valid = false;
+		
+		try
+		{
+			Double.parseDouble(sampleDouble);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			popup.displayText("You need to type in a double - " + sampleDouble + " is not a valid answer.");
+		}
+		return valid;
+	}
 
 
 }
